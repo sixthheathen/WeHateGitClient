775c6f275280c8ac77b320ac880d28668944e457
sixth <kukharea@gmail.com>
2023-04-18 23:19:40 +0300

    Fixed thing a.

---
+++ src/main/java/org/smart/naming/wehategitclient/Mind.java
@@ -0,0 +1,9 @@
+package org.smart.naming.wehategitclient;
+
+/**
+ *
+ * @author LVK
+ */
+public class Mind {
+    
+}
---
+++ src/main/java/org/smart/naming/wehategitclient/WeHateGitClient.java
@@ -0,0 +1,12 @@
+package org.smart.naming.wehategitclient;
+
+/**
+ *
+ * @author sixth
+ */
+public class WeHateGitClient {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
